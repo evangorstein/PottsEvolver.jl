3ca661c0ee573996fafe0416d0e866873f951342
PierreBarrat <p.barrat@live.fr>
2024-09-03 16:56:12 +0200

    getting better -- need to fix sampling numerical sequences

--- src/PottsEvolver.jl
+++ src/PottsEvolver.jl
@@ -1,5 +1,43 @@
 module PottsEvolver
 
-# Write your package code here.
+using BioSequenceMappings
+using Random
+using StatsBase
+using TreeTools
+
+export hamming, symbols # from BioSequenceMappings
+
+import Base: ==, hash
+import Base: copy, show, size, write
+import Base: getindex, setindex!
+
+import BioSequenceMappings: Alignment, to_string, hamming
+
+include("pottsgraph.jl")
+export PottsGraph
+
+include("codons.jl")
+export codon_alphabet, aa_alphabet, nt_alphabet
+export bases, genetic_code
+export isstop, isgap
+
+include("sequences.jl")
+export AbstractSequence, AASequence, CodonSequence
+export Alignment
+export translate
+
+include("sampling.jl")
+export mcmc_sample, SamplingParameters
+
+include("IO.jl")
+export read_graph, read_potts_graph
+
+#=
+- codons.jl: alphabets and genetic code
+- sequences.jl: contain only a vector of Int (or two for CodonSequence).
+  Conversion is done through alphabets
+
+
+=#
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,19 @@ using PottsEvolver
 using Test
 
 @testset "PottsEvolver.jl" begin
-    # Write your tests here.
+    @testset "Codons" begin
+        include("codons/test.jl")
+    end
+
+    @testset "PottsGraph" begin
+        include("pottsgraph/test.jl")
+    end
+
+    @testset "Sequence" begin
+        include("sequences/test.jl")
+    end
+
+    @testset "Sampling" begin
+        include("sampling/test.jl")
+    end
 end
